3850ba0dee13db20246ac22ca68f677007cb3cbb
Hyeonae Jang <aeae_1@naver.com>
2022-02-20 20:36:24 +0900

    Set up spring project

---
+++ spring/notinote/notinote/src/main/java/com/answer/notinote/NotinoteApplication.java
@@ -0,0 +1,13 @@
+package com.answer.notinote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotinoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotinoteApplication.class, args);
+	}
+
+}
---
+++ spring/notinote/notinote/src/test/java/com/answer/notinote/NotinoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.answer.notinote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotinoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
