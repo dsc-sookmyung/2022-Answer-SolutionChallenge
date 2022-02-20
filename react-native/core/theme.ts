393ffa858853379d1797db3dcef7492abbc73ecd
0hee0 <suhee0206@gmail.com>
2022-02-21 01:41:12 +0900

    style: add theme colors

--- react-native/core/theme.ts
+++ react-native/core/theme.ts
@@ -5,11 +5,13 @@ export const theme = {
   colors: {
     ...DefaultTheme.colors,
     primary: '#333D79',
-    accent: '#FAEBEF',
-    secondary: '#C4C4C4',
+    secondary: '#FAEBEF',
     text: '#343a40',
     surface: '#fff',
     background: '#fff',
     error: '#f13a59',
+    coral: '#FF6B6B',
+    skyblue: '#D0E1FC',
+    gray: '#dddddd',
   },
 };
