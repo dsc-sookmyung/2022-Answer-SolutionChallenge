82b04941ee620621195565f9d9cc231cd9d07fb6
0hee0 <suhee0206@gmail.com>
2022-03-04 19:55:06 +0900

    [#11] Feat: Add event to highlighted text

--- react-native/screens/TranslateScreen.tsx
+++ react-native/screens/TranslateScreen.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, TouchableOpacity, ImageBackground, Dimensions } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, ImageBackground, Dimensions, Text, 
+	ToastAndroid, Platform, Alert } from 'react-native';
 import { Camera } from 'expo-camera';
 import { Ionicons } from '@expo/vector-icons';
 import { theme } from '../core/theme';
@@ -8,6 +9,7 @@ import AppLoading from 'expo-app-loading';
 import useFonts from '../hooks/useFonts'
 import SwipeUpDown from 'react-native-swipe-up-down';
 import BottomDrawer from '../components/BottomDrawer';
+import { useToast, Box } from 'native-base';
 
 /* TODO:
 	- 스크롤 내려가게 하기 (지금은 ScrollView의 스크롤이 안 먹음)
@@ -27,6 +29,8 @@ export default function TranslateScreen({ navigation }: Navigation) {
 	const [showTranslated, setShowTranslated] = useState<boolean>(true);
 	const [isFullDrawer, setFullDrawer] = useState<boolean>(false);
 
+	const toast = useToast();
+
 	const LoadFontsAndRestoreToken = async () => {
 		await useFonts();
 	};
@@ -38,6 +42,19 @@ export default function TranslateScreen({ navigation }: Navigation) {
 		})();
 		extractText
 	}, []);
+
+	useEffect(() => {
+		if (results?.filter(result => result.highlight === true).length > 0) {
+			const message = "You can add a schedule to the calendar by clicking the highlighted text."
+			toast.show({	// Design according to mui toast guidelines (https://material.io/components/snackbars#anatomy)
+				render: () => {
+					return <Box bg="rgba(0,0,0,0.8)" px="3" py="2" rounded="xl" mx={2} mb={12} shadow={2}>
+							{message}
+						</Box>;
+				}
+			});
+		}
+	}, [results])
 
 	// if (hasPermission === null) {
 	// 	return <View />;
@@ -68,12 +85,13 @@ export default function TranslateScreen({ navigation }: Navigation) {
 		// TODO: api
 		// TEST
 		setResults([
-			{"content": "Buy Suyeon a delicious meal.", "highlight": false},
-			{"content": "The graduation ceremony will be held in the auditorium at 2 p.m. on February 14th.", "highlight": true},
+			{"id": 1, "content": "Buy Suyeon a delicious meal.", "highlight": false},
+			{"id": 2, "content": "The graduation ceremony will be held in the auditorium at 2 p.m. on February 14th.", "highlight": true},
 		]);
 		setFullText({ 
-			"translated": "You have to buy Suyeon a delicious meal. Hee is writing ... The graduation ceremony will be held in the auditorium at 2 p.m. on February 14th. We look forward to your involvement! You have to buy Suyeon a delicious meal. Hee is writing ... The graduation ceremony will be held in the auditorium at 2 p.m. on February 14th. We look forward to your involvement! You have to buy Suyeon a delicious meal. Hee is writing ... The graduation ceremony will be held in the auditorium at 2 p.m. on February 14th. We look forward to your involvement!", 
-			"korean": "수연이에게 맛있는 밥을 사야합니다. 희가 쓰는 중... 졸업식은 2월 14일에 강당에서 열릴 예정입니다. 많은 참여 부탁드립니다!" 
+			"id": 1, 
+			"translated": "We wish you good health and happiness in your family in the hopeful new year of the 17th graduation ceremony in 2020. Thank you very much for supporting our educational activities and for your constant interest and cooperation in the midst of the 2020 COVID-19 crisis. The 17th graduation ceremony of Hwaam High School will be held in classrooms to prevent the spread of COVID-19. Parents and outsiders are not allowed to enter each class. Please understand that we cannot bring you together because it is an inevitable measure to prevent the spread of infectious diseases. Please congratulate and encourage graduates who are taking their first steps toward a hopeful tomorrow. -Information on the 17th graduation ceremony -1. Date: January 5, 2021 (Tue) 10:00 2. Location: Classroom 3: Contents: Graduation ceremony is held through video clips of each class * Graduates are allowed / Parents and outsiders are not allowed * Graduates must wear masks, self-diagnosis, and go to school.", 
+			"korean": "2020학년도 제17회 졸업식 실시 안내 희망찬 새해를 맞이하여 학부모님의 가정에 건강과 행복이 함께 하시기를 기원합니다. 2020학년도 코로나19라는 위기 상황 속에서 본교 교육활동을 지지해주시고 변함없는 관심과 협조를 보내 주셔서 진심으로 감사드립니다. 화암고등학교 제17회 졸업식은 코로나19 확산 예방을 위해 각반 교실에서 진행합니다. 각반 교실에는 학부모 및 외부인의 출입이 불가능합니다. 감염병 확산 방지를 위한 불가피한 대책이니 함께 자리에 모시지 못함을 양해 부탁드립니다. 희망찬 내일을 향해 첫발을 내딛는 졸업생들에게 멀리서나마 축하와 격려를 보내주시기 바랍니다. -제17회 졸업식 안내- 1. 일시: 2021년 1월 5일(화) 10:00 2. 장소: 각반 교실 3: 내용: 각반 영상으로 졸업식 실시 * 졸업생 출입 가능 / 학부모 및 외부인 출입 불가 * 졸업생은 반드시 마스크 착용, 자가 진단 후 등교" 
 		});
 	}
 	
@@ -83,7 +101,7 @@ export default function TranslateScreen({ navigation }: Navigation) {
 
 	const saveResults = (): void => {
 		// TODO: api
-		alert('saved');
+		Alert.alert("The scanned result was saved in <Database>.");
 	}
 
 	const handleTranslatedText = (): void => {
@@ -116,7 +134,7 @@ export default function TranslateScreen({ navigation }: Navigation) {
 									showFullText={showFullText}
 									showTranslated={showTranslated}
 									isFullDrawer={isFullDrawer}
-									save={true}
+									isTranslateScreen={true}
 									handleFullText={handleFullText}
 									saveResults={saveResults}
 									handleTranslatedText={handleTranslatedText}
@@ -131,7 +149,7 @@ export default function TranslateScreen({ navigation }: Navigation) {
 									showFullText={showFullText}
 									showTranslated={showTranslated}
 									isFullDrawer={isFullDrawer}
-									save={true}
+									isTranslateScreen={true}
 									handleFullText={handleFullText}
 									saveResults={saveResults}
 									handleTranslatedText={handleTranslatedText}
