16c8bae7cc5ff61540bb81c81b93e1ff72548abe
0hee0 <suhee0206@gmail.com>
2022-03-06 23:30:08 +0900

    [#1] Feat: Implement Google sign in

---
+++ react-native/env.d.ts
@@ -0,0 +1,4 @@
+declare module '@env' {
+    export const GOOGLE_CLIENT_ID_WEB: string;
+    export const ENV: 'dev' | 'prod';
+};
--- react-native/screens/LoginScreen.tsx
+++ react-native/screens/LoginScreen.tsx
@@ -1,125 +1,106 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, KeyboardAvoidingView, Alert, Image, Platform } from 'react-native';
-import { FormControl, Input, Button, Stack, VStack, Text } from 'native-base';
-import { emailValidator, passwordValidator } from '../core/utils';
+import React, { useEffect } from 'react';
+import { StyleSheet, View, KeyboardAvoidingView, Image, Platform, TouchableOpacity } from 'react-native';
+import { Button, Text } from 'native-base';
 import { theme } from '../core/theme';
 import type { Navigation } from '../types';
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+import { GOOGLE_CLIENT_ID_WEB } from '@env';
+
+WebBrowser.maybeCompleteAuthSession();
 
   
 export default function LoginScreen({ navigation }: Navigation) {
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
+	const [request, response, promptAsync] = Google.useAuthRequest({
+		expoClientId: GOOGLE_CLIENT_ID_WEB,
+		webClientId: GOOGLE_CLIENT_ID_WEB,
+		// responseType: 'id_token'
+	})
 
-  const errorAlert = (error: string) =>
-    Alert.alert(                    
-      "Login Failed",                 
-      error,                      
-      [
-        { text: "OK", onPress: () => console.log("OK Pressed") }
-      ]
-    );
+	useEffect(() => {
+		// WebBrowser.dismissAuthSession();
+		if (response?.type === 'success') {
+		const { authentication } = response;
+		// console.log('success!')
+		console.log(authentication);
+		// console.log(response.params);
+		// TODO: fetch API
+		// TEST
+		let status = 'join';
+		switch(status) {
+			case 'login':   // if account exists
+			navigation.navigate('Home');
+			case 'join':    // if no account
+			navigation.navigate('Join');
+		}
+		}
+		else {
+			console.log('fail')
+			console.log(response)
+		}
+	}, [response]);
 
-  const onLoginPressed = () => {
-    const emailError = emailValidator(email);
-    const passwordError = passwordValidator(password);
+	const onLoginPressed = () => {
+		navigation.navigate('Home');
+	};
 
-    if (emailError || passwordError) {
-      errorAlert(emailError ? emailError : passwordError);
-      return;
-    }
+	return (
+		<KeyboardAvoidingView style={styles.container} behavior={Platform.OS === "ios" ? "padding" : "height"}>
+		<View style={styles.topView}>
+			<Image
+			style={styles.loginImage}
+			source={require('../assets/images/favicon.png')}
+			/>
+			<Text fontFamily="heading" fontWeight={700} fontStyle="normal" fontSize="4xl" pt={5}>Sign in</Text>
+		</View>
 
-    navigation.navigate('Home');
-  };
+		<View style={styles.bottomView}>
+			{/* TEST: 바로 Home 화면 입장하기 위한 버튼 */}
+			<Button
+				size="lg"
+				onPress={onLoginPressed}
+			>
+				Home
+			</Button>
 
-  return (
-    <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === "ios" ? "padding" : "height"}>
-      <View style={styles.topView}>
-        <Image
-          style={styles.loginImage}
-          source={require('../assets/images/favicon.png')}
-        />
-				<Text fontFamily="heading" fontWeight={700} fontStyle="normal" fontSize={36} pt={5}>Sign in</Text>
-      </View>
+			<TouchableOpacity 
+				activeOpacity={0.5} 
+				style={{ alignItems: 'center' }}
+				disabled={!request}
+				onPress={() => {
+					promptAsync();
+				}}
+			>
+				{/* <Image
+					style={{ transform: [{ scale: 0.75 }] }}
+					source={require('../assets/images/btn_google_signin_dark_normal.png')}
+				/> */}
+				<Text mt={4}>Sign In With Google</Text>
+			</TouchableOpacity>
+		</View>
+		</KeyboardAvoidingView>
+	)
+	};
 
-      <View style={styles.bottomView}>
-        <VStack space={4}>
-          <FormControl>
-            <Input 
-              size="md"
-              placeholder="Email"
-              value={email}
-              onChangeText={(text) => setEmail(text)}
-              autoFocus
-              autoCapitalize="none"
-              returnKeyType={"next"}
-            />
-          </FormControl>
-          <FormControl>
-            <Input 
-              size="md"
-              placeholder="Password"
-              type="password" 
-              value={password}
-              onChangeText={(text) => setPassword(text)}
-            />
-          </FormControl>
-        </VStack>
-        <Button 
-          style={{ alignSelf: 'flex-end' }}
-          // colorScheme="text"
-          variant="ghost"
-          onPress={() => navigation.navigate('ForgotPassword')}
-        >
-          Forgot Password?
-        </Button>
-        <Button mt="8" onPress={onLoginPressed}>
-          Login
-        </Button>
-
-        <Button 
-          onPress={() => navigation.navigate('Join')}
-          variant="ghost"
-        >
-          <Stack direction={{ base: 'row' }}>
-            <Text style={{ color: theme.colors.text }}>Don't have an account? </Text>
-            <Text style={{ fontWeight: '700', color: theme.colors.primary }}>Sign up</Text>
-          </Stack>
-        </Button>
-      </View>
-    </KeyboardAvoidingView>
-  )
-};
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 20,
-    backgroundColor: theme.colors.background,
-    flex: 1,
-    flexDirection: 'column',
-  },
-  topView: {
-    flex: 1, 
-    alignItems: 'center', 
-    justifyContent: 'center'
-  },
-  bottomView: {
-    flex: 1,
-    paddingBottom: 40
-  },
-  loginImage: {
-    width: 50,
-    height: 50,
-  },
-  title: {
-    paddingTop: 30,
-    fontWeight: '700',
-    fontSize: 36,
-    fontFamily: 'Lora_700Bold',
-  },
-  forgotPassword: {
-    width: '100%',
-    alignItems: 'flex-end',
-    marginBottom: 24,
-  },
+	const styles = StyleSheet.create({
+	container: {
+		padding: 20,
+		backgroundColor: theme.colors.background,
+		flex: 1,
+		flexDirection: 'column',
+	},
+	topView: {
+		flex: 1, 
+		alignItems: 'center', 
+		justifyContent: 'center'
+	},
+	bottomView: {
+		flex: 1,
+		paddingBottom: 40
+	},
+	loginImage: {
+		width: 50,
+		height: 50
+	},
 });
   
