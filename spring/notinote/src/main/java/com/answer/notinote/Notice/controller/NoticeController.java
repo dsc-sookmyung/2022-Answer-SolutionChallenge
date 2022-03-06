dd32b3d3c1ec3f8423165f65acc1de9c08e7fa89
hellouz818 <hellouz818@gmail.com>
2022-03-06 19:48:00 +0900

    [#6] feat: Add Google Cloud Vision OCR API

--- spring/notinote/src/main/java/com/answer/notinote/Notice/controller/NoticeController.java
+++ spring/notinote/src/main/java/com/answer/notinote/Notice/controller/NoticeController.java
@@ -1,13 +1,36 @@
 package com.answer.notinote.Notice.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gcp.vision.CloudVisionTemplate;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class NoticeController {
+
+    @Autowired private ResourceLoader resourceLoader;
+    @Autowired private CloudVisionTemplate cloudVisionTemplate;
+
     @RequestMapping(value = "/notice/ocr", method = RequestMethod.POST)
     public String ocr () {
         return "ocr";
     }
+
+    @GetMapping("/extractText")
+    public String extractText(String imageUrl) {
+        // [START spring_vision_text_extraction]
+        String textFromImage =
+                this.cloudVisionTemplate.extractTextFromImage(this.resourceLoader.getResource(imageUrl));
+        return "Text from image: " + textFromImage;
+    }
+
+
+
+
 }
