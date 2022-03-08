fc1f0acc0bc22ad1f7935e3d4b4bc730177d14e4
hellouz818 <hellouz818@gmail.com>
2022-03-08 16:19:04 +0900

    [#6] feat: Detect text in Requested image file

--- spring/notinote/src/main/java/com/answer/notinote/Notice/controller/NoticeController.java
+++ spring/notinote/src/main/java/com/answer/notinote/Notice/controller/NoticeController.java
@@ -9,9 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.util.UUID;
+import java.io.IOException;
+
 
 
 @RestController
@@ -24,28 +23,18 @@ public class NoticeController {
     @Autowired
     NoticeService noticeService;
 
-    @RequestMapping(value = "/notice/ocr", method = RequestMethod.POST)
-    public String ocr (@RequestParam MultipartFile uploadfile, HttpServletRequest request) throws Exception{
-        String nimagename = uploadfile.getOriginalFilename();
-        String uuid = UUID.randomUUID().toString();
-        nimagename = uuid + nimagename;
-        String uploadPath = System.getProperty("user.dir")+"/src/main/resources/static";
-        File saveFile = new File(uploadPath, nimagename);
-        try {
-            uploadfile.transferTo(saveFile);
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-        return "ocr";
+    public NoticeController(NoticeService noticeService) {
+        this.noticeService = noticeService;
     }
 
-    @GetMapping("/extractText")
-    public String extractText(String imageUrl) {
-        // [START spring_vision_text_extraction]
-        String textFromImage =
-                this.cloudVisionTemplate.extractTextFromImage(this.resourceLoader.getResource(imageUrl));
-        return "Text from image: " + textFromImage;
+    @RequestMapping(value = "/notice/ocr", method = RequestMethod.POST)
+    public String saveImage (@RequestPart MultipartFile uploadfile) throws IOException {
+        String nimageurl  = noticeService.saveImage(uploadfile);
+        String koreantext = noticeService.detectText(nimageurl);
+
+        return "Text from image: " + koreantext;
     }
+
 
 
 
--- spring/notinote/src/main/java/com/answer/notinote/Notice/service/NoticeService.java
+++ spring/notinote/src/main/java/com/answer/notinote/Notice/service/NoticeService.java
@@ -1,10 +1,19 @@
 package com.answer.notinote.Notice.service;
 
-import com.answer.notinote.Notice.domain.entity.Notice;
+
 import com.answer.notinote.Notice.domain.repository.NoticeRepository;
+import com.google.cloud.vision.v1.*;
+import com.google.protobuf.ByteString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 
 @Service
@@ -12,6 +21,59 @@ public class NoticeService {
 
     @Autowired
     NoticeRepository noticeRepository;
+
+    public String saveImage(MultipartFile uploadfile){
+        String nimagename = uploadfile.getOriginalFilename();
+        String uuid = UUID.randomUUID().toString();
+        nimagename = uuid + nimagename;
+        String uploadPath = System.getProperty("user.dir")+"/src/main/resources/static";
+        try {
+            File saveFile = new File(uploadPath, nimagename);
+            uploadfile.transferTo(saveFile);
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return uploadPath+'/'+nimagename;
+    }
+
+    public String detectText(String filePath) throws IOException{
+        List<AnnotateImageRequest> requests = new ArrayList<>();
+
+        ByteString imgBytes = ByteString.readFrom(new FileInputStream(filePath));
+
+        Image img = Image.newBuilder().setContent(imgBytes).build();
+        Feature feat = Feature.newBuilder().setType(Feature.Type.TEXT_DETECTION).build();
+        AnnotateImageRequest request =
+                AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
+        requests.add(request);
+        ArrayList <String> text = new ArrayList<String>();
+
+
+        try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
+            BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
+            List<AnnotateImageResponse> responses = response.getResponsesList();
+
+
+            for (AnnotateImageResponse res : responses) {
+                if (res.hasError()) {
+                    System.out.format("Error: %s%n", res.getError().getMessage());
+                    return "error";
+                }
+
+                for (EntityAnnotation annotation : res.getTextAnnotationsList()) {
+                    text.add(String.format("%s", annotation.getDescription()));
+
+
+                }
+            }
+            System.out.println("Text : "+text.get(0));
+        }
+        return text.get(0);
+    }
+
+
 
 
 }
