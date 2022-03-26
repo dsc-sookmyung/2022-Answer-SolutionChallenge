370ba5bdde48575a4c8572b74f99656705dbb8cb
장현애 <aeae_1@naver.com>
2022-03-26 16:13:02 +0900

    feat: extract event from notice

--- spring/notinote/src/main/java/com/answer/notinote/Notice/dto/NoticeTitleListDto.java
+++ spring/notinote/src/main/java/com/answer/notinote/Notice/dto/NoticeTitleListDto.java
@@ -3,11 +3,24 @@ package com.answer.notinote.Notice.dto;
 import com.answer.notinote.Notice.domain.entity.Notice;
 import lombok.Getter;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 public class NoticeTitleListDto {
+    private String uploadfile;
     private String title;
+    private LocalDate date;
+    private List<NoticeSentenceDto> fullText = new ArrayList<>();
+    private String korean;
 
-    public NoticeTitleListDto(Notice entity){
+    public NoticeTitleListDto(Notice entity, List<NoticeSentenceDto> sentences){
+        this.uploadfile = entity.getNimageurl() + "/" + entity.getNimagename();
         this.title = entity.getTitle();
+        this.date = entity.getNdate();
+        this.korean = entity.getOrigin_full();
+
+        this.fullText.addAll(sentences);
     }
 }
