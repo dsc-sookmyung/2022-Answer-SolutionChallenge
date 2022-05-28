eecd325bc0e06a6eb01bae142326a690fcf242fb
Hyeonae <aeae_1@naver.com>
2022-05-28 21:24:28 +0900

    [#5] fix: add AuthController and refreshToken api
    - userController에서 authController 분리
    - jwtToken -> accessToken으로 명칭 변경
    - /refresh api 추가

--- spring/notinote/src/main/java/com/answer/notinote/Exception/ErrorCode.java
+++ spring/notinote/src/main/java/com/answer/notinote/Exception/ErrorCode.java
@@ -10,8 +10,8 @@ public enum ErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 계정이 존재하지 않습니다."),
     USER_DUPLICATED(HttpStatus.CONFLICT, "이미 해당 계정의 유저가 존재합니다."),
     TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "토큰이 존재하지 않습니다."),
-    TOKEN_INVALID(HttpStatus.BAD_REQUEST, "액세스 토큰이 만료되었습니다."),
-    NOT_SUPPORTED_TYPE(HttpStatus.CONFLICT, "지원하지 않는 로그인 형식입니다"),
+    TOKEN_EXPIRED(HttpStatus.BAD_REQUEST, "액세스 토큰이 만료되었습니다."),
+    TOKEN_NOT_EXPIRED(HttpStatus.BAD_REQUEST, "액세스 토큰이 만료되지 않았습니다."),
     NOT_FOUND(HttpStatus.NOT_FOUND, "해당 객체가 존재하지 않습니다."),
     ;
 
