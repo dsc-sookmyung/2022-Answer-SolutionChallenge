1db52d599ab6e818babe1638323f1533f9951055
mori8 <kaithape@gmail.com>
2022-06-01 15:13:29 +0900

    Feat: extract event from notices by using RoBERTa

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,28 +1,106 @@
+from transformers import pipeline
+import parsedatetime as pdt
 from fastapi import FastAPI
 from pydantic import BaseModel
+import re
 
-from translated_events import event_list
-from school_events_extractor import SchoolEventsExtractor
+from translated_events import event_list_kr, event_list_en, event_list_km, event_list_th, event_list_zh, event_list_ja, event_list_vi
 
 
 class Request(BaseModel):
     language: str
     kr_text: str
+    en_text: str
     translated_text: str
 
 
 app = FastAPI()
+cal = pdt.Calendar()
+qa_pipeline = pipeline(
+    "question-answering",
+    model="deepset/roberta-base-squad2",
+    tokenizer="deepset/roberta-base-squad2"
+)
+events = []
+NOT_FOUNDED = -1
 
 
-@app.post("/event-dict")
+def make_event_dict_by_lang(lang):
+    if lang == 'en':
+        return dict(zip(event_list_en, event_list_en))
+    elif lang == 'kr':
+        return dict(zip(event_list_en, event_list_kr))
+    elif lang == 'th':
+        return dict(zip(event_list_en, event_list_th))
+    elif lang == 'km':
+        return dict(zip(event_list_en, event_list_km))
+    elif lang == 'vi':
+        return dict(zip(event_list_en, event_list_vi))
+    elif lang == 'ja':
+        return dict(zip(event_list_en, event_list_ja))
+    elif lang == 'zh':
+        return dict(zip(event_list_en, event_list_zh))
+
+
+def ask_model(context, question):
+    response = qa_pipeline({
+        'context': context,
+        'question': question
+    })
+    answer = response["answer"]
+    return answer
+
+
+def format_date(date):
+    date.replace(" ", "")
+    date.replace("/", "-")
+    date.replace(".", "-")
+    # delete day information inside brackets e.g.(Mon)
+    if date.find("(") != NOT_FOUNDED and date.find(")") != NOT_FOUNDED:
+        open_index = date.find("(")
+        close_index = date.find(")")
+        date = date[:open_index] + date[close_index + 1:]
+    return date
+
+
+def has_alpha(text):
+    reg = re.compile(r'[a-zA-Z]')
+    if reg.match(text):
+        return True
+    return False
+
+
+@app.post("/event")
 async def root(request: Request):
-    events_extractor = SchoolEventsExtractor(request.kr_text, request.translated_text, event_list, request.language)
-    events_extractor.find_all_events_starting_index()
+    en_to_tr_event_dict = make_event_dict_by_lang(request.language)
+    for event in event_list_en:
+        if request.en_text.find(event) != NOT_FOUNDED:
+            question = 'When is the {}?'.format(event)
+            answer = ask_model(request.en_text, question)
+            extracted_date = format_date(answer)
 
-    if events_extractor.get_number_of_events_in_kr_text() < 1:
-        return {"status": 200, "message": "no events"}
+            try:
+                if has_alpha(extracted_date):
+                    datetime = cal.parseDateText(extracted_date)
+                else:
+                    datetime = cal.parseDate(extracted_date)
+                datetime = str(datetime[0]) + "-" + str(datetime[1]).zfill(2) + "-" + str(datetime[2]).zfill(2)
+            except:
+                return {"status": 200, "message": "no date information"}
 
-    events_extractor.find_all_dates_from_korean_text()
-    events_extractor.match_dates_with_events_and_save()
+            translated_event = en_to_tr_event_dict[event]
+            translated_event_start_index = request.translated_text.lower().find(translated_event.lower())
+            translated_event_end_index = translated_event_start_index + len(translated_event)
 
-    return {'status': 200, 'body': events_extractor.results}
+            if translated_event_start_index == NOT_FOUNDED:
+                return {"status": 200, "message": "translation error"}
+
+            result = {
+                "event": en_to_tr_event_dict[event],
+                "s_index": translated_event_start_index,
+                "e_index": translated_event_end_index,
+                "date": datetime
+            }
+
+            events.append(result)
+    return {"status": 200, "body": events}
